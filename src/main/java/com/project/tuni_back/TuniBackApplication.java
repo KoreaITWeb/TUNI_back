258b9ee60f281858347bba4d14aa02aaff1dcd96
unknown <gimsejong991@gmail.com>
2025-07-18 19:16:47 +0900

    Initial commit

---
+++ src/main/java/com/project/tuni_back/TuniBackApplication.java
@@ -0,0 +1,13 @@
+package com.project.tuni_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuniBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuniBackApplication.class, args);
+	}
+
+}
