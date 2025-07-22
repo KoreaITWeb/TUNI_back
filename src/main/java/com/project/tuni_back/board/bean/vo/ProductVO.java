db879046dd6bcad634127490a32d257dc9d6ce25
sungjongmoon <atw180908@gmail.com>
2025-07-22 21:42:31 +0900

    상품등록

---
+++ src/main/java/com/project/tuni_back/board/bean/vo/ProductVO.java
@@ -0,0 +1,11 @@
+package com.project.tuni_back.board.bean.vo;
+
+import lombok.Data;
+
+@Data
+public class ProductVO {
+    private int id;
+    private String name;
+    private String description;
+    private int price;
+}
---
+++ src/main/java/com/project/tuni_back/board/dao/ProductDAO.java
@@ -0,0 +1,27 @@
+package com.project.tuni_back.board.dao;
+
+import com.project.tuni_back.board.mapper.ProductMapper;
+import com.project.tuni_back.board.bean.vo.ProductVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDAO {
+
+    @Autowired
+    ProductMapper mapper;
+
+    public List<ProductVO> getAll() {
+        return mapper.getAllProducts();
+    }
+
+    public ProductVO getById(int id) {
+        return mapper.getProductById(id);
+    }
+
+    public void insert(ProductVO vo) {
+        mapper.insertProduct(vo);
+    }
+}
