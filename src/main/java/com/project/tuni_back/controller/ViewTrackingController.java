ddd327fd16b9012771bba2b81bae54276964502a
김도현 <babi03ummi@gmail.com>
2025-08-01 20:39:00 +0900

    조회수 파일 업로드

--- src/main/java/com/project/tuni_back/controller/ViewTrackingController.java
+++ src/main/java/com/project/tuni_back/controller/ViewTrackingController.java
@@ -1,11 +1,25 @@
 package com.project.tuni_back.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.project.tuni_back.bean.dao.ViewTrackingDAO;
+import com.project.tuni_back.bean.vo.ViewTrackingVO;
+
+@RestController
 @RequestMapping("/views/*")
 public class ViewTrackingController {
-
+	@Autowired
+	private ViewTrackingDAO dao;
+	
+	@PostMapping("tracking")
+	public ResponseEntity<Void> tracking(@RequestBody ViewTrackingVO vo) {
+		dao.views(vo);
+		return ResponseEntity.ok().build();
+	}
 	
 }
