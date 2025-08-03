51a126f8bbb319d0e1c20b370e09a90c6bf3bb57
김세종 <gimsejong991@gmail.com>
2025-08-03 22:28:54 +0900

    내가 등록한 상품 구현

--- src/main/java/com/project/tuni_back/mapper/BoardMapper.java
+++ src/main/java/com/project/tuni_back/mapper/BoardMapper.java
@@ -1,14 +1,19 @@
 package com.project.tuni_back.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import com.project.tuni_back.bean.vo.BoardVO;
 
+import io.lettuce.core.dynamic.annotation.Param;
+
 @Mapper
 public interface BoardMapper {
 	public List<BoardVO> getProductList(Long schoolId);
+	public List<BoardVO> getProductList(String userId);
+	//public List<BoardVO> getProductList(Map<String, Object> params);
 	public BoardVO readProduct(Long boardId);
 	public int registerProduct(BoardVO vo);
 	public int removeProduct(Long boardId);
@@ -17,4 +22,5 @@ public interface BoardMapper {
 	public int updateViews(Long boardId);
 	public int incrementLikes(Long boardId);
 	public int decrementLikes(Long boardId);
+	
 }
